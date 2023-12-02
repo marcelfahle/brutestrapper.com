c0c984e80397a6468b0fee908bf84896be76f247
Marcel Fahle <m.fahle@gmail.com>
2023-12-02 05:35:29 +0100

    plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import PlausibleProvider from 'next-plausible'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <PlausibleProvider domain="brutestrapper.com">
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </PlausibleProvider>
   )
 }
