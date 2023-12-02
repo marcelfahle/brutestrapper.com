2e28fc910a9f258d102a34aaabf7aa70de7fac6d
Marcel Fahle <m.fahle@gmail.com>
2023-12-02 05:30:05 +0100

    escape all the things

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,27 @@ export default function Home() {
           <strong>Brutestrapper: Noun - </strong><i>broo−t−strap−er</i> - A relentless warrior in the startup arena, armed with nothing but grit, brains, and an unshakable will. This is not your average bootstrapper. This is bootstrapping with a vengeance, with brute force.
         </p>
 
-        <p><strong>1. We Embrace the Grind:</strong> We know that success isn't handed out like free samples at a grocery store. It’s wrestled from the jaws of failure, with blood, sweat, and a healthy dose of cuss words.</p>
+        <p><strong>1. We Embrace the Grind:</strong> We know that success isn&apos;t handed out like free samples at a grocery store. It&apos;s wrestled from the jaws of failure, with blood, sweat, and a healthy dose of cuss words.</p>
 
-        <p><strong>2. No Bullshit Allowed:</strong> Our bullshit detectors are finely tuned instruments. We cut through the crap, the hype, and the noise. If it doesn't add value, it's out.</p>
+        <p><strong>2. No Bullshit Allowed:</strong> Our bullshit detectors are finely tuned instruments. We cut through the crap, the hype, and the noise. If it doesn&apos;t add value, it&apos;s out.</p>
 
-        <p><strong>3. Resourcefulness is our Middle Name:</strong> Limited resources? That's just another way of saying "it's time to get creative." We don't need fancy tools; we need a problem, a brain, and maybe some duct tape.</p>
+        <p><strong>3. Resourcefulness is our Middle Name:</strong> Limited resources? That&apos;s just another way of saying &quot;it&apos;s time to get creative.&quot; We don&apos;t need fancy tools; we need a problem, a brain, and maybe some duct tape.</p>
 
-        <p><strong>4. Failure is Our Fuel:</strong> We eat setbacks for breakfast. Each failure is a stepping stone, a bloody badge of honor, a story to tell. We don't give up; we get up, learn, and hit back harder.</p>
+        <p><strong>4. Failure is Our Fuel:</strong> We eat setbacks for breakfast. Each failure is a stepping stone, a bloody badge of honor, a story to tell. We don&apos;t give up; we get up, learn, and hit back harder.</p>
 
-        <p><strong>5. Our Network is Our Net Worth:</strong> We know the value of a strong network. It's not about schmoozing; it's about genuine connections, helping others, and building a community of badass Brutestrappers.</p>
+        <p><strong>5. Our Network is Our Net Worth:</strong> We know the value of a strong network. It&apos;s not about schmoozing; it&apos;s about genuine connections, helping others, and building a community of badass Brutestrappers.</p>
 
-        <p><strong>6. Learning is Non-Negotiable:</strong> We’re autodidacts, constantly learning, evolving, and adapting. If we don’t know it, we’ll learn it. If it doesn’t exist, we’ll create it.</p>
+        <p><strong>6. Learning is Non-Negotiable:</strong> We&apos;re autodidacts, constantly learning, evolving, and adapting. If we don’t know it, we&apos;ll learn it. If it doesn’t exist, we&apos;ll create it.</p>
 
         <p><strong>7. Perfection is Procrastination in Disguise:</strong> We believe in action over perfection. Done is better than perfect. We iterate, we improve, but first and foremost, we launch.</p>
 
         <p><strong>8. We’re Fueled by Passion, not Praise:</strong> We don’t work for accolades or pats on the back. Our drive comes from within, from the burning need to create, to solve, to build.</p>
 
-        <p><strong>9. Our Work Speaks Louder than Our Words:</strong> We let our achievements do the talking. We’re not here to brag; we’re here to deliver.</p>
+        <p><strong>9. Our Work Speaks Louder than Our Words:</strong> We let our achievements do the talking. We&apos;re not here to brag; we&apos;re here to deliver.</p>
 
-        <p><strong>10. We’re in it for the Long Haul:</strong> This isn’t a sprint; it’s a marathon... with hurdles... and fire... and possibly sharks. But we’re here for it, every step of the way.</p>
+        <p><strong>10. We&apos;re in it for the Long Haul:</strong> This isn&apos;t a sprint; it’s a marathon... with hurdles... and fire... and possibly sharks. But we’re here for it, every step of the way.</p>
 
-        <p>In conclusion, being a Brutestrapper means being a relentless, resourceful, and resilient force in the startup world. It's about embracing the hard, the uncertain, and the downright scary parts of creating something from nothing. So here’s to the Brutestrappers, the dreamers, the doers, the relentless warriors of the startup world. Keep pushing, keep fighting, and remember – if it were easy, everyone would do it.</p>
+        <p>In conclusion, being a Brutestrapper means being a relentless, resourceful, and resilient force in the startup world. It&apos;s about embracing the hard, the uncertain, and the downright scary parts of creating something from nothing. So here&apos;s to the Brutestrappers, the dreamers, the doers, the relentless warriors of the startup world. Keep pushing, keep fighting, and remember – if it were easy, everyone would do it.</p>
 
         <p>Now go out there and brute force your way to success. 🚀💪🔥</p>
       </article>
